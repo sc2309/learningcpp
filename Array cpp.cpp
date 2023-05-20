56b5d69d81ee457d732aa9ea8351b4a75c418050
aananyarai <134141562+aananyarai@users.noreply.github.com>
2023-05-20 16:59:02 +0530

    cpp array file
    cpp array file

---
+++ Array cpp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+  int main ()
+  {      
+     cout<< "Enter any 10 numbers under 100 . "<< endl;
+     
+     int myNumbers [10];
+      int i;
+     
+      for (i=1; i<10; i++) {  
+      cin>> myNumbers[i];
+	  }
+     
+     return 0;
+     
+	 }
